c11f5949a50af4f90a9caec07a2811944ce21970
Maarten Postma <maarten.postma1000@gmail.com>
2024-07-12 02:13:14 +0200

    Add all styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,56 @@ import checkIcon from "./assets/check.png";
 import notIcon from "./assets/minus.png";
 
 function App() {
-  return (
-      <>
-        <header>
-          <h1>Tech it easy dashboard</h1>
-        </header>
-        <main>
-          <section className="sales-overview">
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-          </section>
-          <section className="featured-display">
-
-          </section>
-        <img src={bestSellingTv.sourceImg}></img>
-        </main>
-        <p className="tvsSold">Total tvs sold: {soldTelivision()}</p>
-        <p className="tvsPurchaded">Total tvs purchased: {totalPurchases()}</p>
-        <p className="tvtobesold">Total tvs to be sold {tvtobesold()}</p>
-        <p>{nameOfTelevision()}</p>
-        <p>{priceoftelivision()}</p>
-        <p>{telivisionSizes()}</p>
-        <p>
-          <img className="icons" src={checkIcon} alt="check-icon"/>
-          wifi
-          <img className="icons" src={notIcon} alt="not-icon"/>
-          speech
-          <img className="icons" src={checkIcon} alt="check-icon"/>
-          hdr
-          <img className="icons" src={checkIcon} alt="check-icon"/>
-          bluetooth
-          <img className="icons" src={notIcon} alt="not-icon"/>
-          ambilight</p>
+  return (<>
+        <div className="wrapper">
+          <header>
+            <h1>Tech it easy dashboard</h1>
+          </header>
+          <main>
+            <section className="sales-overview">
+              <h2>Verkoopoverzicht</h2>
+              <div className="box-wrapper">
+                <div className="box tv-sold">
+                  <p>Aantal verkochte producten</p>
+                  <p>{soldTelivision()}</p>
+                </div>
+                <div className="box tv-purchased">
+                  <p>Aantal ingekochte producten</p>
+                  <p>{totalPurchases()}</p>
+                </div>
+                <div className="box tv-to-be-sold">
+                  <p>Aantal te verkopen producten</p>
+                  <p>{tvtobesold()}</p>
+                </div>
+              </div>
+            </section>
+            <section className="featured-display">
+              <h2>Best verkochte tv</h2>
+              <div className="card">
+                <div className="left-col">
+                  <img className="featured-img"
+                       src={bestSellingTv.sourceImg}></img>
+                </div>
+                <div className="right-col">
+                  <h3>{nameOfTelevision()}</h3>
+                  <strong className="price">{priceoftelivision()}</strong>
+                  <p className="sizes">{telivisionSizes()}</p>
+          <p>
+            <img className="icons" src={checkIcon} alt="check-icon"/>
+            wifi
+            <img className="icons" src={notIcon} alt="not-icon"/>
+            speech
+            <img className="icons" src={checkIcon} alt="check-icon"/>
+            hdr
+            <img className="icons" src={checkIcon} alt="check-icon"/>
+            bluetooth
+            <img className="icons" src={notIcon} alt="not-icon"/>
+            ambilight</p>
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
       </>
 
   )
